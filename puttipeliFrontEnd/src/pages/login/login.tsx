493fab5bd7d856c0a68f4c05f2c7323f2af591b2
Jimi Jukkala <jukkalajimi@gmail.com>
2023-12-25 21:05:21 +0200

    Added login form to the login page

--- puttipeliFrontEnd/src/pages/login/login.tsx
+++ puttipeliFrontEnd/src/pages/login/login.tsx
@@ -1,5 +1,33 @@
+import "./loginpageStyles.css"
+
 const LoginPage = () => {
-    return <h1>This is the login page</h1>
+  const login = (event: React.SyntheticEvent) => {
+    // TODO IMPLEMENT LOGIN LOGIC
+    event.preventDefault()
+  }
+
+  return (
+    <>
+      <div id='loginMaincontainer'>
+        <h1 id='loginpageHeader'>Kirjaudu sisään</h1>
+        <form onSubmit={login} id='loginForm'>
+          <div>
+            <input type='text' placeholder='Käyttäjätunnus' />
+          </div>
+
+          <div>
+            <input type='password' placeholder='Salasana' />
+          </div>
+
+          <div>
+            <input type='checkbox' /> Muista minut
+          </div>
+
+          <button type='submit'>Kirjaudu</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default LoginPage
