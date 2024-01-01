3520cb88b35ac97372d658a75992dc6321101a8c
Jimi Jukkala <jukkalajimi@gmail.com>
2024-01-01 22:09:08 +0200

    Added mock data and new route

--- puttipeliBackEnd/src/routes/user.ts
+++ puttipeliBackEnd/src/routes/user.ts
@@ -1,10 +1,33 @@
 // all of the user logic here
-import express from 'express';
+import express from "express"
+import { User } from "./types"
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (_req, res) => {
-    res.send("This is the user router")
+// Mock data
+const users = [
+  {
+    name: "Jimi",
+    dob: new Date("2001-04-17"),
+    registered: "1.1.2024",
+    games: [],
+  },
+]
+
+router.get("/", (_req, res) => {
+  res.send("This is the user router")
 })
 
-export default router;
+router.get("/:id", (req, res) => {
+  const id = req.params.id
+
+  // Send user data
+  if (users[Number(id)] != null) {
+    res.json(users[Number(id)])
+  }
+
+  // No user case
+  res.status(404).send("No user with that id")
+})
+
+export default router
