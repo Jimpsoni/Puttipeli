184aa77d55e38c78fff7a9a2697cb496d7a2c8dc
Jimi Jukkala <jukkalajimi@gmail.com>
2024-01-03 22:24:44 +0200

    Initialized login

--- puttipeliBackEnd/src/routes/login.ts
+++ puttipeliBackEnd/src/routes/login.ts
@@ -1,10 +1,33 @@
 // all of the login logic here
-import express from 'express';
+import express from "express"
+import { checkLoginCredit } from "../services/loginService"
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (_req, res) => {
-    res.send("This is the login router")
+router.get("/info", (_req, res) => {
+  res.send("This is the login router")
 })
 
-export default router;
+router.put("/", (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  if (!username) {
+    res.status(400).send("Missing username")
+    return
+  }
+
+  if (!password) {
+    res.status(400).send("Missing password")
+    return
+  }
+
+  if (!checkLoginCredit(username, password)) {
+    res.status(400).send("Incorrect username or password")
+    return
+  }
+
+  res.sendStatus(200)
+})
+
+export default router
