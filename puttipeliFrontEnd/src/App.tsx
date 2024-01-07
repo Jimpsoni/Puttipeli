40289d21254c40456d3a7dfd121d6a5f6d07f650
Jimi Jukkala <jukkalajimi@gmail.com>
2024-01-07 21:09:43 +0200

    Added the welcome page to app.tsx

--- puttipeliFrontEnd/src/App.tsx
+++ puttipeliFrontEnd/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import MainPage from "./pages/mainpage"
 import LoginPage from "./pages/login/login"
 import Register from "./pages/register/register"
+import WelcomePage from "./pages/welcomepage/welcomePage"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' Component={MainPage} />
         <Route path='/login' Component={LoginPage} />
         <Route path='/register' Component={Register} />
+        <Route path='/puttipeli' Component={WelcomePage} />
       </Routes>
     </>
   )
