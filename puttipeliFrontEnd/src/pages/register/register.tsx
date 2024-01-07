5f6e50fd79e6319ac1e037b05a17827de69ea38c
Jimi Jukkala <jukkalajimi@gmail.com>
2024-01-07 21:10:12 +0200

    Initialised Welcome page

--- puttipeliFrontEnd/src/pages/register/register.tsx
+++ puttipeliFrontEnd/src/pages/register/register.tsx
@@ -1,5 +1,63 @@
+import { useState } from "react"
+import './styles.css'
+
+
 const Register = () => {
-  return <h1>this is the register page</h1>
+  const registerNewUser = () => {
+    console.log("New user registered")
+  }
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordAgain, setPasswordAgain] = useState('')
+
+
+  return (
+    <div id='loginMaincontainer'>
+    <h1 id='loginpageHeader'>Luo uusi käyttäjätunnus</h1>
+    <form onSubmit={registerNewUser} id='loginForm'>
+      <div>
+        <input
+          className='textInput'
+          type='text'
+          placeholder='Käyttäjätunnus'
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value)
+          }}
+        />
+      </div>
+
+      <div id="passwordInput">
+        <input
+          className='textInput'
+          type='password'
+          placeholder='Salasana'
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+        />
+      </div>
+
+      <div>
+        <input
+          className='textInput'
+          type='password'
+          placeholder='Salasana uudelleen'
+          value={passwordAgain}
+          onChange={(e) => {
+            setPasswordAgain(e.target.value)
+          }}
+        />
+      </div>
+
+      <button type='submit' className='submitButton'>
+        Rekisteröidy
+      </button>
+    </form>
+  </div>
+  )
 }
 
 export default Register
---
+++ puttipeliFrontEnd/src/pages/welcomepage/welcomePage.tsx
@@ -0,0 +1,5 @@
+const WelcomePage = () => {
+    return <h1>Hello world</h1>
+}
+
+export default WelcomePage
