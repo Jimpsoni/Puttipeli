1eb2950af3a2b7e409d5c18681b0207eb4773c42
Jimi Jukkala <jukkalajimi@gmail.com>
2024-01-10 18:52:02 +0200

    Adding header as a utility component

---
+++ puttipeliFrontEnd/src/utilitycomponents/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./styles.css"
+
+/*
+  ehdotuksia:
+    - Sivuluettelo, jossa asetukset yms
+*/
+
+const Header = () => {
+  return (
+    <div id='Header'>
+      <h2 id='HeaderHeader'>Puttipeli</h2>
+    </div>
+  )
+}
+
+export default Header
