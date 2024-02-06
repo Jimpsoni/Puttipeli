e46eb5c8e441a41e933563546dd25350532d2d29
Jimi Jukkala <jukkalajimi@gmail.com>
2024-02-06 15:51:43 +0200

    Minor fixes

--- puttipeliBackEnd/src/app.ts
+++ puttipeliBackEnd/src/app.ts
@@ -2,9 +2,14 @@ import express from "express"
 import loginRouter from "./routes/login"
 import userRouter from "./routes/user"
 import cors from "cors"
+import path from "path"
 
 const app = express()
 app.use(express.json())
+
+// Serve all the static files
+app.use(express.static(path.join(__dirname, "../../puttipeliFrontEnd/dist")))
+
 app.use(cors())
 
 // All of the routes
