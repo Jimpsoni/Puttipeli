70938aee69555b9227681055216797c7742f19e3
Jimi Jukkala <jukkalajimi@gmail.com>
2024-02-08 22:05:03 +0200

    Improved registeration route

--- puttipeliBackEnd/src/routes/registeration.ts
+++ puttipeliBackEnd/src/routes/registeration.ts
@@ -4,13 +4,12 @@ import { AddNewUser } from "../services/UserService/userService"
 const router = express.Router()
 
 // CREATE NEW USER
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const data = req.body
+  const response = await AddNewUser({ ...data })
   
-  AddNewUser({...data})
-
-
-  res.status(201).send()
+  if (response.status === "ok") res.status(201).send()
+  else if (response.status === "error") res.status(400).json({'errors': response.errors})
 })
 
 export default router
