a69ef14f2b4b2051f8190f0e7c1db47ca89d4be5
Jimi Jukkala <jukkalajimi@gmail.com>
2024-02-08 19:23:16 +0200

    Added new user type

--- puttipeliBackEnd/src/types.ts
+++ puttipeliBackEnd/src/types.ts
@@ -1,12 +1,17 @@
 export interface UserType {
-    id: string
-    username: string
-    passwordHash: string
-    dob: Date
-    registered: Date
-    games: Game[]
+  id: string
+  username: string
+  passwordHash: string
+  email: string
+  registered: Date
+  games: Game[]
 }
 
+export interface NewUserType {
+  username: string
+  passwordHash: string
+  email: string
+}
 
 // For now, games only have scores and gamemodes
 export interface Game {
