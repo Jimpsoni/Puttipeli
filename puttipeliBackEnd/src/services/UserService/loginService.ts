fe54e6066eca2c58d6f52ccb4fafe939fddd554f
Jimi Jukkala <jukkalajimi@gmail.com>
2024-02-09 17:14:16 +0200

    Implemented Login Logic

--- puttipeliBackEnd/src/services/UserService/loginService.ts
+++ puttipeliBackEnd/src/services/UserService/loginService.ts
@@ -1,6 +1,29 @@
+import { User } from "./userSchema"
+import mongoose from "mongoose"
 
-export const checkLoginCredit = (username: string, password: string): boolean => {
-    console.log(username)
-    console.log(password)
-    return true
+export const checkLoginCredit = async (
+  username: string,
+  password: string
+): Promise<boolean> => {
+  await mongoose.connect(process.env.DB_URI as string)
+
+  try {
+    const user = await User.findOne({ username: `${username}` })
+
+    if (!user) {
+      mongoose.connection.close()
+      return false
+    }
+
+    // @ts-ignore   User must have passwordHash in DB
+    if (user.password === password) {
+      mongoose.connection.close()
+      return true
+    }
+
+    mongoose.connection.close()
+    return false
+  } catch (e) {
+    return false
+  }
 }
