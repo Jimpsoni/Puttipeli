7a89332dbd6ede43149b39db336bbe86e21df9ef
Jimi Jukkala <jukkalajimi@gmail.com>
2024-02-09 17:53:17 +0200

    Renamed user.ts -> users.ts

--- puttipeliBackEnd/src/routes/users.ts
+++ puttipeliBackEnd/src/routes/users.ts
@@ -1,5 +1,6 @@
 // all of the user logic here
 import express from "express"
+import { getAllUsers } from "../services/UserService/userService"
 
 const router = express.Router()
 
@@ -7,9 +8,15 @@ router.get("/", (_req, res) => {
   res.send("This is the user router. use /:id to get users")
 })
 
-router.get("/:id", (req, _res) => {
+router.get("/all", async (_req, res) => {
+  const users = await getAllUsers()
+  res.status(200).json(users)
+})
+
+router.get("/:id", (req, res) => {
   const id = req.params.id
   console.log(id)
+  res.status(404).send()
 })
 
 export default router
