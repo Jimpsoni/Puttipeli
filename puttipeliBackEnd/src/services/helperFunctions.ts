3d6b96abfc54808a46533130859606dd2f79ee5f
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-02 23:49:00 +0200

    function to check if object is user

--- puttipeliBackEnd/src/services/helperFunctions.ts
+++ puttipeliBackEnd/src/services/helperFunctions.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import { UserType } from "../types"
 
 export async function HashPassword(pass: string): Promise<string> {
   if (pass.length < 6) return ""
@@ -12,4 +13,27 @@ export async function checkPassword(
   compareTo: string
 ): Promise<boolean> {
   return await bcrypt.compare(compareTo, password)
+}
+
+export function checkIfObjectIsUser(o: unknown): UserType | null {
+  if (!(typeof o === 'object') || !o) return null
+  
+  if ('id' in o && typeof o.id == 'string' &&
+      'username' in o && typeof o.username == 'string' &&
+      'password' in o && typeof o.password == 'string' &&
+      'email' in o && typeof o.email == 'string' &&
+      'registered' in o && o.registered instanceof Date && 
+      'games' in o
+  ) {
+    return {
+      id: o.id,
+      username: o.username,
+      password: o.password,
+      email: o.email,
+      registered: o.registered,
+      games: o.games as Game[]
+    }
+  } else { 
+    return null
+  }
 }
