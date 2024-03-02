6c18ea327a84a39e73fdfb4b089c2c91b19bee31
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-02 23:49:22 +0200

    Removed ts nocheck

--- puttipeliBackEnd/tests/routes/user.test.ts
+++ puttipeliBackEnd/tests/routes/user.test.ts
@@ -1,11 +1,12 @@
-// @ts-nocheck
-
 import request from "supertest"
 import app from "../../src/app"
 import mongoose from "mongoose"
 import { User } from "../../src/services/UserService/userSchema"
 import { AddNewUser } from "../../src/services/UserService/userService"
 
+// It is not possible that saved_user is anything else than usertype
+// eslint-disable-next-line
+// @ts-ignore
 let saved_user
 
 beforeAll(async () => {
@@ -18,11 +19,13 @@ beforeAll(async () => {
   try {
     await mongoose.connect(process.env.DB_URI as string)
     await User.collection.drop()
-    await AddNewUser({ ...user }).then(response => {
-      saved_user = response.user
+    await AddNewUser({ ...user }).then((response) => {
+      if (response.status == "ok") {
+        saved_user = response.user
+      }
     })
   } catch (e) {
-    throw Error("Something wrong with MongoDB")
+    throw Error("Something wrong with MongoDB..")
   }
 }, 30000)
 
@@ -48,6 +51,9 @@ describe("Returning users from Database", () => {
   })
 
   test("right id returns user", async () => {
+    // It is not possible that saved_user is anything else than usertype
+    // eslint-disable-next-line
+    // @ts-ignore
     const res = await request(app).get(`/api/users/${saved_user.id}`)
     expect(res.status).toEqual(200)
   })
