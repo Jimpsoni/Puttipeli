8dbb06706f703739054eec90476d693c39ccbfbe
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-03 21:41:43 +0200

    Added better status codes to login

--- puttipeliBackEnd/src/routes/login.ts
+++ puttipeliBackEnd/src/routes/login.ts
@@ -8,31 +8,43 @@ router.get("/", (_req, res) => {
 })
 
 router.post("/", (req, res) => {
+  // TODO don't send password hash to user
   const username = req.body.username as string
   const password = req.body.password as string
 
   if (!username) {
-    res.status(401).json({ error: "Missing username" })
+    res.status(200).json({ status: 200, error: "Missing username" })
     return
   }
 
   if (!password) {
-    res.status(401).json({ error: "Missing password" })
+    res.status(401).json({ status: "error", error: "Missing password" })
     return
   }
 
   checkLoginCredit(username, password)
-    .then(response => {
-      if (response.status === 'ok') {
-        res.sendStatus(200).json
-      }
-      else if (response.status === 'error') {
-        if (response.error === 'Internal Server Error') res.status(500).json({ error: response.error })
-        if (response.error === "No user with that username") res.status(401).json({ error: "Could not find user with that username" })
-        if (response.error === "Password didn't match") res.status(401).json({ error: "Incorrect username or password" })
+    .then((response) => {
+      if (response.status === "ok") {
+        res.status(200).send({ status: "ok", user: response.user })
+      } else if (response.status === "error") {
+        if (response.error === "Internal Server Error")
+          res.status(500).json({ status: "error", error: response.error })
+        if (response.error === "No user with that username")
+          res.status(401).json({
+            status: "error",
+            error: "Could not find user with that username",
+          })
+        if (response.error === "Password didn't match")
+          res
+            .status(401)
+            .json({ status: "error", error: "Incorrect username or password" })
       }
     })
-    .catch(() => res.status(500).json({ error: "Something wrong with server" }))
+    .catch(() =>
+      res
+        .status(500)
+        .json({ status: "error", error: "Something wrong with server" })
+    )
 })
 
 export default router
