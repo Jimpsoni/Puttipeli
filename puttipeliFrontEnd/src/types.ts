c7a007ff5e7d6f09925835fc20353d6cbcc4eaea
remoneve <remonenveeti@gmail.com>
2024-03-15 15:48:11 +0200

    fix types.ts

--- puttipeliFrontEnd/src/types.ts
+++ puttipeliFrontEnd/src/types.ts
@@ -1,16 +1,16 @@
 export interface UserType {
-    id: string
-    username: string
-    email: string
-    registered: Date
-    games: Game[]
-  }
-  
-export interface Game {
-    gamemode: string
-    score: number
+  id: string
+  username: string
+  email: string
+  registered: Date
+  games: Game[]
 }
-  
+
+export interface Game {
+  gamemode: string
+  score: number
+}
+
 export interface GameResult {
   distance: number,
   shotsInBasket: number
