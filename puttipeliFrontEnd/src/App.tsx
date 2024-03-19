2478c328c5da625883074c233ed9e87b695a82dc
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-19 16:54:07 +0200

    Added context to the app

--- puttipeliFrontEnd/src/App.tsx
+++ puttipeliFrontEnd/src/App.tsx
@@ -1,20 +1,29 @@
+// React
 import { Route, Routes } from "react-router-dom"
+import userContext from "./services/userContext.ts"
+
+// Pages
 import MainPage from "./pages/mainpage"
 import LoginPage from "./pages/login/login"
 import Register from "./pages/register/register"
 import WelcomePage from "./pages/welcomepage/welcomePage"
 import Game from "./pages/game/Game"
+import { useState } from "react"
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <>
-      <Routes>
-        <Route path='/' Component={MainPage} />
-        <Route path='/login' Component={LoginPage} />
-        <Route path='/register' Component={Register} />
-        <Route path='/puttipeli' Component={WelcomePage} />
-        <Route path='/uusi_peli' Component={Game} />
-      </Routes>
+      <userContext.Provider value={[user, setUser]}>
+        <Routes>
+          <Route path='/' Component={MainPage} />
+          <Route path='/login' Component={LoginPage} />
+          <Route path='/register' Component={Register} />
+          <Route path='/puttipeli' Component={WelcomePage} />
+          <Route path='/uusi_peli' Component={Game} />
+        </Routes>
+      </userContext.Provider>
     </>
   )
 }
