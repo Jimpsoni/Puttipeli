dfc2ed52b7d33da33df1e33bff236869be4685a8
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-20 15:27:28 +0200

    Changed so if user doesn't have games, show "No games"

--- puttipeliFrontEnd/src/pages/welcomepage/welcomePage.tsx
+++ puttipeliFrontEnd/src/pages/welcomepage/welcomePage.tsx
@@ -1,8 +1,8 @@
-import Header from '../../utilitycomponents/Header'  // Utility component
-import GameTab from '../../utilitycomponents/GameTab/GameTab'
-import { useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
-import userContext from '../../services/userContext'
+import Header from "../../utilitycomponents/Header" // Utility component
+import GameTab from "../../utilitycomponents/GameTab/GameTab"
+import { useNavigate } from "react-router-dom"
+import { useContext, useEffect } from "react"
+import userContext from "../../services/userContext"
 import "./styles.css"
 
 /*
@@ -15,42 +15,69 @@ import "./styles.css"
       siihen (vko, kk, vv, alltime)?
 */
 
-
-const recentGame = {
-  date: new Date(),
-  points: 339,
-  hitpercent: 59
+interface ShowGameInterface {
+  games: string[]
 }
 
-const bestGame = {
-  date: new Date(),
-  points: 459,
-  hitpercent: 76
+const ShowGames = (props: ShowGameInterface) => {
+  if (props.games.length < 1) {
+    return (
+      <div className='tabContainer games'>
+        <div>Ei vielä pelejä näytettäväksi</div>
+      </div>
+    )
+  }
+  
+  // TODO hae käyttäjältä pelin tiedot
+  const recentGame = {
+    date: new Date(),
+    points: 339,
+    hitpercent: 59,
+  }
+
+  // TODO hae käyttäjältä pelin tiedot
+  const bestGame = {
+    date: new Date(),
+    points: 459,
+    hitpercent: 76,
+  }
+
+  return (
+    <div className='tabContainer games'>
+      <h3 className='gamesSubHeader'>Viimeisin</h3>
+      <GameTab data={recentGame} />
+      <h3 className='gamesSubHeader'>Viikon paras</h3>
+      <GameTab data={bestGame} />
+    </div>
+  )
 }
 
 const WelcomePage = () => {
   const nav = useNavigate()
   const [user, setUser] = useContext(userContext)
   const StartNewGame = () => {
-    nav('/uusi_peli')
+    nav("/uusi_peli")
   }
+
+  useEffect(() => {
+    if (user == null) {
+      nav("/login")
+    }
+  }, [])
 
   return (
     <div id='mainContainer'>
-      <Header/>
-      <h2>Hei {user.username}!</h2>
+      <Header />
+      <h2>Hei {user && user.username}!</h2>
 
       <div className='tabContainer'>
-        <div className="button">Aiemmat pelit</div>
-        <div className="button" onClick={StartNewGame}>Aloita uusi peli</div>
+        <div className='button'>Aiemmat pelit</div>
+        <div className='button' onClick={StartNewGame}>
+          Aloita uusi peli
+        </div>
       </div>
 
-      <div className='tabContainer games'>
-        <h3 className='gamesSubHeader'>Viimeisin</h3>
-        <GameTab data={recentGame}/>
-        <h3 className='gamesSubHeader'>Viikon paras</h3>
-        <GameTab data={bestGame}/>
-      </div>
+      <ShowGames games={user.games} />
     </div>
   )
 }
