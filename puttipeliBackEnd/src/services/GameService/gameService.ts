bf3fde542fb8434cc1b1947f521679a14b379b9f
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-22 21:04:10 +0200

    Saving the game to user

--- puttipeliBackEnd/src/services/GameService/gameService.ts
+++ puttipeliBackEnd/src/services/GameService/gameService.ts
@@ -1,10 +1,24 @@
-// Placeholder
+// @ts-nocheck
 
+import mongoose from "mongoose"
 import { GameRequest } from "../../types"
+import { User } from "../UserService/userSchema"
+
+mongoose.set("strictQuery", false)
 
 export const saveGameToUser = async (props: GameRequest) => {
-  // Just a placeholder now
-  await new Promise(() => "Hello World")
-  console.log(props)
-  return
+  try {
+    await mongoose.connect(process.env.DB_URI as string)
+    await mongoose.connection.syncIndexes()
+
+    const query = await User.findOne({ _id: props.userid })
+    if (!query) throw new Error("No user with that ID")
+    console.log(props.game)
+    query.games = query.games.concat(props.game)
+    await query.save()
+
+    return
+  } finally {
+    await mongoose.connection.close()
+  }
 }
