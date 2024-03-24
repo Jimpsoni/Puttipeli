4e963b96c4872f2c7fd339e8b30b26650f1ed338
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-24 18:50:03 +0200

    Created GameSchema

--- puttipeliBackEnd/src/services/GameService/gameSchema.ts
+++ puttipeliBackEnd/src/services/GameService/gameSchema.ts
@@ -1,4 +1,28 @@
-import mongoose from "mongoose"
-import { Game } from "../../types"
+import mongoose, { Schema } from "mongoose"
+import { GameType } from "../../types"
 
-const gameSchema = new mongoose.Schema<Game>
+const gameSchema = new mongoose.Schema<GameType>({
+  userid: {
+    type: Schema.Types.ObjectId
+  },
+  points: {
+    type: Number,
+    min: 0,
+    max: 1000,
+  },
+  rounds: [Object],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+gameSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+export const Game = mongoose.model<GameType>("Game", gameSchema)
