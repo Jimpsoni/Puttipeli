96df9b695cba1db1315d50b411416fbaae939d7e
Jimi Jukkala <jukkalajimi@gmail.com>
2024-03-24 18:50:32 +0200

    Fixed tests

--- puttipeliBackEnd/tests/routes/game.test.ts
+++ puttipeliBackEnd/tests/routes/game.test.ts
@@ -23,10 +23,12 @@ beforeAll(async () => {
   }
 }, 30000)
 
-describe("Saving games to db", () => {
+describe("Sending request to save game to db", () => {
   it("Sending game without userid", async () => {
     const game = {
-      game: [],
+      points: 250,
+      rounds: [],
+      date: new Date(Date.now())
     }
     const res = await request(app).post("/api/game/submit").send(game)
 
@@ -34,20 +36,48 @@ describe("Saving games to db", () => {
     expect(res.text).toEqual("Could not find 'userid' in request")
   }, 10000)
 
-  it("Sending game without game", async () => {
+  it("Sending game without 'rounds'", async () => {
     const game = {
-      userid: saved_user.id,
+      userid: "NewID",
+      points: 250,
+      date: new Date(Date.now())
     }
     const res = await request(app).post("/api/game/submit").send(game)
 
     expect(res.status).toEqual(400)
-    expect(res.text).toEqual("Could not find 'game' in request")
+    expect(res.text).toEqual("Could not find 'rounds' in request")
+  }, 10000)
+
+  it("Sending game without 'date'", async () => {
+    const game = {
+      userid: "NewID",
+      points: 250,
+      rounds: [],
+    }
+    const res = await request(app).post("/api/game/submit").send(game)
+
+    expect(res.status).toEqual(400)
+    expect(res.text).toEqual("Could not find 'date' in request")
+  }, 10000)
+
+  it("Sending game without 'points'", async () => {
+    const game = {
+      userid: "NewID",
+      date: new Date(Date.now()),
+      rounds: [],
+    }
+    const res = await request(app).post("/api/game/submit").send(game)
+
+    expect(res.status).toEqual(400)
+    expect(res.text).toEqual("Could not find 'points' in request")
   }, 10000)
 
   it("Sending game that is not array", async () => {
     const game = {
       userid: saved_user.id,
-      game: "Score: 200",
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: "Score: 200",
     }
 
     const res = await request(app).post("/api/game/submit").send(game)
@@ -59,7 +89,9 @@ describe("Saving games to db", () => {
   it("Sending game that not length 20", async () => {
     const game = {
       userid: saved_user.id,
-      game: [{ distance: 2, shotsInBasket: 2 }],
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [{ distance: 2, shotsInBasket: 2 }],
     }
 
     const res = await request(app).post("/api/game/submit").send(game)
@@ -71,7 +103,9 @@ describe("Saving games to db", () => {
   it("Sending game that is longer than 20", async () => {
     const game = {
       userid: saved_user.id,
-      game: [
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [
         { distance: 2, shotsInBasket: 2 },
         { distance: 2, shotsInBasket: 2 },
         { distance: 2, shotsInBasket: 2 },
@@ -105,7 +139,9 @@ describe("Saving games to db", () => {
   it("Sending game that string instead of int", async () => {
     const game = {
       userid: saved_user.id,
-      game: [
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [
         { distance: 2, shotsInBasket: "2" },
         { distance: 2, shotsInBasket: 2 },
         { distance: 2, shotsInBasket: 2 },
@@ -137,7 +173,9 @@ describe("Saving games to db", () => {
   it("Sending game that has illegal key", async () => {
     const game = {
       userid: saved_user.id,
-      game: [
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [
         { distance: 2, shotsInBaskets: 2 },
         { distance: 2, shotsInBasket: 2 },
         { distance: 2, shotsInBasket: 2 },
@@ -169,7 +207,9 @@ describe("Saving games to db", () => {
   it("Sending game that has illegal key", async () => {
     const game = {
       userid: saved_user.id,
-      game: [
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [
         { distance: 2, shotsInBaskets: 2 },
         { distance: 2, shotsInBasket: 2 },
         { distance: 2, shotsInBasket: 2 },
@@ -201,7 +241,9 @@ describe("Saving games to db", () => {
   it("Game with incorrect id is not accepted", async () => {
     const game = {
       userid: new mongoose.Types.ObjectId(),
-      game: [
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [
         { distance: 10, shotsInBasket: 5 },
         { distance: 10, shotsInBasket: 5 },
         { distance: 10, shotsInBasket: 5 },
@@ -233,7 +275,9 @@ describe("Saving games to db", () => {
   it("Game with correct values is accepted", async () => {
     const game = {
       userid: saved_user.id,
-      game: [
+      date: new Date(Date.now()),
+      points: 200,
+      rounds: [
         { distance: 10, shotsInBasket: 5 },
         { distance: 10, shotsInBasket: 5 },
         { distance: 10, shotsInBasket: 5 },
